b87552296b4018d5594f146d9d09a2be9c7a629c
Divya Sawant <86359301+DivyaRsawant@users.noreply.github.com>
2023-04-13 11:41:03 +0530

    Add files via upload

---
+++ PositiveNegative.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a number"))
+if num > 0:
+    print("Number",num,"is positive")
+elif num == 0:
+    print("The number ",num,"is 0")
+else:
+    print("number ",num," is negative")
